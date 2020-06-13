2884d89910841a38b736579c3b3c527da31bb251
Sazeda Sultana <sazedas@uci.edu>
2020-06-12 17:10:31 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# sazedas.github.io" 
---
+++ index.js
@@ -0,0 +1,97 @@
+
+/* Curriculum Vitae page*/
+const CV = ()=> {
+    window.open("Sazeda Sultana Curriculum Vitae.pdf");
+}
+
+/* Resume page */
+const resume = ()=>{
+    window.open("Sazeda_Sultana_Resume.pdf");
+}
+
+
+/* Portfolio page - UX/UI */
+const Competitive_analysis= ()=>{
+    window.open("Proposal and Competitive Analysis.pdf");
+}
+const Paper_Browser_Tool= ()=>{
+    window.open("https://www.figma.com/file/SkFhnnEUZklWyGju353UDT/Paper-Browser-Tool?node-id=0%3A1");
+}
+const Understanding_users= ()=>{
+    window.open("Understanding Your Users, Potential Users, and Other Stakeholders.pdf");
+}
+const usability= ()=>{
+    window.open("Identify Usability Issues.pdf");
+}
+const Redesign= ()=>{
+    window.open("Redesign.pdf");
+}
+const complete_UX_UI_redesign= ()=>{
+    window.open("Heuristic Evaluation.pdf");
+}
+const Paciolan_UX_UI_redesign= ()=>{
+    window.open("Paciolan's Web App System Design.pdf");
+}
+
+
+/* Portfolio page - Web Design */
+const PeekPeek= ()=>{
+    window.open("http://dev-peekpeek.com.s3-website-us-west-1.amazonaws.com/");
+}
+const Paper_Browser_Tool_Web_design= ()=>{
+    window.open("https://depstein.github.io/personalinformatics-metareview/");
+}
+const Paciolan_Web_Design= ()=>{
+    window.open("...");
+}
+
+/* Portfolio page - Research */
+const thesis= ()=>{
+    window.open("UCI_Honors_Thesis.pdf");
+}
+const published_paper= ()=>{
+    window.open("...");
+}
+
+
+
+/* Portfolio page - Software Requirements Engineering */
+const Stakeholder_model = ()=>{
+    window.open("Stakeholder Model and Elicitation Documentation.pdf");
+}
+const Goal_Oriented_Analysis_Model= ()=>{
+    window.open("Goal-Oriented Analysis Model.pdf");
+}
+const Usage_Model= ()=>{
+    window.open("Usage Model - Scenarios and Use Cases.pdf");
+}
+const Domain_model= ()=>{
+    window.open("Domain Model - Class Diagrams.pdf");
+}
+const Systems_requirements_specification= ()=>{
+    window.open("Systems Requirements Specification.pdf");
+}
+const Software_validation= ()=>{
+    window.open("Validation or Customer Review.pdf");
+}
+
+/* Portfolio page - Software Design */
+const RoboKid= ()=>{
+    window.open("RoboKid - Design Studio 1.pdf");
+}
+const educational_traffic_flow_simulation_program= ()=>{
+    window.open("Design Studio 2.pdf");
+}
+const History= ()=>{
+    window.open("History - Design Studio 3.pdf");
+}
+
+/* Portfolio page - Software System Design */
+const Paciolan_system_design= ()=>{
+    window.open("Paciolan's Web App System Design.pdf");
+}
+
+/* Portfolio page - Computer Supported Cooperative Work */
+const Google_Drive= ()=>{
+    window.open("Google Drive from Collaborative Perspective.pdf");
+}
