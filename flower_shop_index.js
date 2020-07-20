465269011cf07836c2ddc24263d37b4643fe686f
Sazeda Sultana <sazedas@uci.edu>
2020-07-19 18:50:24 -0700

    added flower_shop_index.js file

---
+++ flower_shop_index.js
@@ -0,0 +1,16 @@
+
+
+const flowerShop1 = ()=> {
+    window.open("https://www.whiteflowerfarm.com/","","width=600,height=600")
+}
+const flowerShop2 = ()=> {
+    window.open(`https://www.floretflowers.com/`,"", "width=600, height=600")
+}
+
+const sallysFlowerCollection = () => {
+    window.open(`flower_collection.html`)
+
+}
+
+
+
