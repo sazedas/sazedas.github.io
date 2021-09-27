990d70d4860c41971f0d56df520e19efd04cc2ce
Sazeda Sultana <sazedas@uci.edu>
2021-09-26 17:06:09 -0700

    changing CV and resume file name

--- index.js
+++ index.js
@@ -1,122 +1,119 @@
 console.log("hello");
 /* Curriculum Vitae page*/
-const CV = ()=> {
-    window.open("Sazeda Sultana Curriculum Vitae.pdf");
-}
+const CV = () => {
+  window.open("Sazeda Sultana - Curriculum Vitae.pdf");
+};
 
 /* Resume page */
-const resume = ()=>{
-    window.open("resume.pdf");
-}
-
+const resume = () => {
+  window.open("Sazeda_Sultana_Resume.pdf");
+};
 
 /* Portfolio page - UX/UI */
-const RobotKid_UX_UI= ()=>{
-    window.open("robotKid_ux_ui.html");
-}
-const Paper_Browser_Tool= ()=>{
-    window.open("https://www.figma.com/file/SkFhnnEUZklWyGju353UDT/Paper-Browser-Tool?node-id=0%3A1");
-}
-const Understanding_users= ()=>{
-    window.open("understanding_users.html");
-}
-const usability= ()=>{
-    window.open("usability.html");
-}
-const Redesign= ()=>{
-    window.open("redesign.html");
-}
-const Paciolan_UX_UI_redesign= ()=>{
-    window.open("Paciolan_UX_UI.html");
-}
-const BubbleSortly = () =>{
-    window.open("https://www.figma.com/file/j6ww43vy2oNMUgghrl05Fz/BubbleSortlyBook.ai?node-id=0%3A1");
-}
-const SuperlyCafeteria = () =>{
-    window.open("https://www.figma.com/file/bqjYHDIbkKWG3jZZAUQnTc/Superly-Cafeteria");
-}
-
+const RobotKid_UX_UI = () => {
+  window.open("robotKid_ux_ui.html");
+};
+const Paper_Browser_Tool = () => {
+  window.open(
+    "https://www.figma.com/file/SkFhnnEUZklWyGju353UDT/Paper-Browser-Tool?node-id=0%3A1"
+  );
+};
+const Understanding_users = () => {
+  window.open("understanding_users.html");
+};
+const usability = () => {
+  window.open("usability.html");
+};
+const Redesign = () => {
+  window.open("redesign.html");
+};
+const Paciolan_UX_UI_redesign = () => {
+  window.open("Paciolan_UX_UI.html");
+};
+const BubbleSortly = () => {
+  window.open(
+    "https://www.figma.com/file/j6ww43vy2oNMUgghrl05Fz/BubbleSortlyBook.ai?node-id=0%3A1"
+  );
+};
+const SuperlyCafeteria = () => {
+  window.open(
+    "https://www.figma.com/file/bqjYHDIbkKWG3jZZAUQnTc/Superly-Cafeteria"
+  );
+};
 
 /* Portfolio page - Interaction Desing */
-const History_Interaction_Design = () =>{
-    window.open(" history_interaction_design.html");
-}
-
+const History_Interaction_Design = () => {
+  window.open(" history_interaction_design.html");
+};
 
 /* Portfolio page - Web Design */
-const PeekPeek= ()=>{
-    window.open("http://dev-peekpeek.com.s3-website-us-west-1.amazonaws.com/");
-}
-const Paper_Browser_Tool_Web_design= ()=>{
-    window.open("http://personal-informatics.depstein.net/");
-}
-const Paciolan_Web_Design= ()=>{
-    window.open("paciolan_search_result_Page.html");
-}
-const Imaginary_Flower_Shop_Sally= () =>{
-    window.open("flower_shop_index.html")
-}
-const titanRadio = ()=>{
-    window.open("https://www.titanradio.org/");
-}
-const titanUniverse = () =>{
-    window.open("https://www.titanuniverse.org/");
-}
+const PeekPeek = () => {
+  window.open("http://dev-peekpeek.com.s3-website-us-west-1.amazonaws.com/");
+};
+const Paper_Browser_Tool_Web_design = () => {
+  window.open("http://personal-informatics.depstein.net/");
+};
+const Paciolan_Web_Design = () => {
+  window.open("paciolan_search_result_Page.html");
+};
+const Imaginary_Flower_Shop_Sally = () => {
+  window.open("flower_shop_index.html");
+};
+const titanRadio = () => {
+  window.open("https://www.titanradio.org/");
+};
+const titanUniverse = () => {
+  window.open("https://www.titanuniverse.org/");
+};
 
 /* Portfolio page - Research */
-const thesis= ()=>{
-    window.open("UCI_honors_thesis.html");
-}
-const published_paper= ()=>{
-    window.open("https://depstein.net/assets/pubs/depstein_imwut20.pdf");
-}
-
+const thesis = () => {
+  window.open("UCI_honors_thesis.html");
+};
+const published_paper = () => {
+  window.open("https://depstein.net/assets/pubs/depstein_imwut20.pdf");
+};
 
 /* Portfolio page - Software Design */
-const Paciolan_software_design= ()=>{
-    window.open("paciolan_software_design.html");
-}
-const History= ()=>{
-    window.open("history.html")
-}
-
+const Paciolan_software_design = () => {
+  window.open("paciolan_software_design.html");
+};
+const History = () => {
+  window.open("history.html");
+};
 
 /* Portfolio page - Software Requirements Engineering */
-const Stakeholder_model = ()=>{
-    window.open("requirement_elicitation_and_stakeholder_model.html");
-}
-const Goal_Oriented_Analysis_Model= ()=>{
-    window.open("goal_oriented_analysis_model.html");
-}
-const Usage_Model= ()=>{
-    window.open("usage_model_scenarios_use_cases.html");
-}
-const Domain_model= ()=>{
-    window.open("domain_model_class_diagrams.html");
-}
-const Systems_requirements_specification= ()=>{
-    window.open("systems_requirements_specification.html");
-}
-const Software_validation= ()=>{
-    window.open("validation_or_customer_review.html");
-}
-
+const Stakeholder_model = () => {
+  window.open("requirement_elicitation_and_stakeholder_model.html");
+};
+const Goal_Oriented_Analysis_Model = () => {
+  window.open("goal_oriented_analysis_model.html");
+};
+const Usage_Model = () => {
+  window.open("usage_model_scenarios_use_cases.html");
+};
+const Domain_model = () => {
+  window.open("domain_model_class_diagrams.html");
+};
+const Systems_requirements_specification = () => {
+  window.open("systems_requirements_specification.html");
+};
+const Software_validation = () => {
+  window.open("validation_or_customer_review.html");
+};
 
 /* Portfolio page - Computer Supported Cooperative Work */
-const Google_Drive= ()=>{
-    window.open("google_drive.html");
-}
+const Google_Drive = () => {
+  window.open("google_drive.html");
+};
 
 /* Portfolio page - Games */
-const JavaScript_Game=() => {
-    window.open("javaScript_game.html");
-}
-const JavaScript_Rain_Game=()=>{
-    window.open("rain_game.html");
-}
-const JavaScript_Number_Guess_Game=()=>{
-    window.open('number_guess_game.html');
-}
-
-
-
+const JavaScript_Game = () => {
+  window.open("javaScript_game.html");
+};
+const JavaScript_Rain_Game = () => {
+  window.open("rain_game.html");
+};
+const JavaScript_Number_Guess_Game = () => {
+  window.open("number_guess_game.html");
+};
